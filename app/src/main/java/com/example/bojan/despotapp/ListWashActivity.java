b97d5e28257450f0ee05c340b56633527f87aaa2
BojanAnox <b.markovic@anoxtech.com>
2016-05-14 20:43:53 +0200

    Implemented search list.

--- app/src/main/java/com/example/bojan/despotapp/ListWashActivity.java
+++ app/src/main/java/com/example/bojan/despotapp/ListWashActivity.java
@@ -2,7 +2,10 @@ package com.example.bojan.despotapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import list_components.CustomBaseAdapter;
@@ -10,6 +13,9 @@ import list_components.CustomBaseAdapter;
 public class ListWashActivity extends AppCompatActivity {
 
     private ListView mListWash;
+    private EditText mSearch;
+
+    private CustomBaseAdapter mCustomBaseAdapter;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -24,7 +30,18 @@ public class ListWashActivity extends AppCompatActivity {
 
         Utils.setBackButtonOnActionBar(getSupportActionBar());
 
+        mSearch = (EditText) findViewById(R.id.id_search);
+        mSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    mCustomBaseAdapter.getFilter().filter(mSearch.getText());
+            }
+        });
+
+        mCustomBaseAdapter = new CustomBaseAdapter(this);
+
         mListWash = (ListView) findViewById(R.id.id_listView);
-        mListWash.setAdapter(new CustomBaseAdapter(this));
+        mListWash.setTextFilterEnabled(true);
+        mListWash.setAdapter(mCustomBaseAdapter);
         }
     }
--- app/src/main/java/list_components/CustomBaseAdapter.java
+++ app/src/main/java/list_components/CustomBaseAdapter.java
@@ -6,6 +6,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import com.example.bojan.despotapp.R;
@@ -17,13 +19,17 @@ import database.DatabaseHelper;
 /**
  * Created by Bojan on 5/4/2016.
  */
-public class CustomBaseAdapter extends BaseAdapter {
+public class CustomBaseAdapter extends BaseAdapter implements Filterable {
 
     ArrayList<SingleRow> mSingleRowArrayList;
+    ArrayList<SingleRow> mHelpSingleRowArrayList;
+
     Context context;
+    ValueFilter valueFilter;
 
     public CustomBaseAdapter(Context context) {
         mSingleRowArrayList = new ArrayList<SingleRow>();
+
         this.context = context;
 
         insertDataInList();
@@ -34,14 +40,15 @@ public class CustomBaseAdapter extends BaseAdapter {
 
         Cursor cursor = databaseHelper.readData();
 
-        if (cursor.getCount() != 0)
-        {
-           if(cursor.moveToFirst())
-               mSingleRowArrayList.add(new SingleRow(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
+        if (cursor.getCount() != 0) {
+            if (cursor.moveToFirst())
+                mSingleRowArrayList.add(new SingleRow(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
 
-            while(cursor.moveToNext())
+            while (cursor.moveToNext())
                 mSingleRowArrayList.add(new SingleRow(cursor.getString(0), cursor.getString(1), cursor.getString(2)));
         }
+
+        mHelpSingleRowArrayList = mSingleRowArrayList;
     }
 
     @Override
@@ -75,6 +82,50 @@ public class CustomBaseAdapter extends BaseAdapter {
         date.setText(singleRow.dates);
 
         return v;
+    }
+
+    @Override
+    public Filter getFilter() {
+        if (valueFilter == null) {
+            valueFilter = new ValueFilter();
+        }
+        return valueFilter;
+    }
+
+    private class ValueFilter extends Filter {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            if (constraint != null && constraint.length() > 0) {
+                ArrayList<SingleRow> filterList = new ArrayList<SingleRow>();
+                for (int i = 0; i < mHelpSingleRowArrayList.size(); i++) {
+                    if ((mHelpSingleRowArrayList.get(i).regNumbers.toUpperCase())
+                            .contains(constraint.toString().toUpperCase())) {
+
+                        SingleRow sr = new SingleRow(mHelpSingleRowArrayList.get(i)
+                                .regNumbers, mHelpSingleRowArrayList.get(i)
+                                .prices, mHelpSingleRowArrayList.get(i)
+                                .dates);
+
+                        filterList.add(sr);
+                    }
+                }
+                results.count = filterList.size();
+                results.values = filterList;
+            } else {
+                results.count = mHelpSingleRowArrayList.size();
+                results.values = mHelpSingleRowArrayList;
+            }
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint,
+                                      FilterResults results) {
+            mSingleRowArrayList = (ArrayList<SingleRow>) results.values;
+            notifyDataSetChanged();
+        }
     }
 }
 
